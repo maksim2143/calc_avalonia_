d86f907303ffe81c3acd4019673bf9f0039b0cd8
dfd <fdf>
2020-09-23 20:39:07 +0300

    Добавьте файлы проекта.

---
+++ AvaloniaApplication1/MainWindow.axaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
+using System.Runtime.InteropServices;
+using System.Threading;
+
+namespace AvaloniaApplication1
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+           
+        }
+        void send(object sender, RoutedEventArgs args)
+        {
+            var button = sender as Button;
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            
+        }
+    }
+}
